596442c8e65817e891775d206d50916ee24a3343
Rodrigo Zietlow <rodrigozietlow@gmail.com>
2015-06-23 17:58:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
+<html>
+	<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+	
+		<div class="container-fluid">
+		
+			<div class="row">
+			
+				<div class="col-lg-4" border=1 id="menuEsquerda">
+				
+				
+				</div>
+				
+				<div class="col-lg-8" bgcolor="red" id="menuDireita">
+				
+					<div class="row">
+						
+						<div class="col-lg-12" id="topoMenuDireita" bgcolor="green">
+					
+						</div>
+						
+					</div>
+				
+					<div class="row">
+					
+						<div class="col-lg-12" id="conteudoMenuDireita">
+						
+						</div>
+					
+					</div>
+					
+					<div class="row">
+					
+						<div class="col-lg-offset-1 col-lg-10" align='center' id="inputTexto">
+						
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+		</div>
+	
+	</body>
+</html>
+
 
