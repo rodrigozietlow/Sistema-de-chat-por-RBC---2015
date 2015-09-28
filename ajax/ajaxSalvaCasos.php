9950e30ca66e65013130adc911ec43792e1eb313
Polkshot <igoreca221@gmail.com>
2015-09-28 10:16:24 -0300

    xml_inserted_cases

--- ajax/ajaxSalvaCasos.php
+++ ajax/ajaxSalvaCasos.php
@@ -1,8 +1,7 @@
 <?phpheader('Content-Type: text/html; charset=ISO-8859-1');$_POST = array_map('strtolower', $_POST);
 require_once($_SERVER['DOCUMENT_ROOT']."/Sistema-de-chat-por-RBC---2015/MySQL.class.php");
 
-$tipo = $_POST['tipo'];
-
+$tipo = $_POST['tipo'];
 if($tipo == 1){
 	$nivel1 = $_POST['nivel1'];
 	$proximo = $_POST['proxima'];
@@ -16,7 +15,7 @@ if($tipo == 1){
 		$query = "UPDATE casos SET peso = peso+1 WHERE id=$id";
 	}
 	else{
-		$query = "INSERT INTO casos(nivel1, proxima, peso) VALUES('$nivel1', '$proximo', 1)";				$xml= simplexml_load_file("xml_casos.xml");		$caso = $xml->addChild("caso");		$caso->addChild("nivel1",$nivel1);		$caso->addChild("peso",1);		$caso->addChild("proxima",$proximo);		file_put_contents("xml_casos.xml",$xml->asXML());
+		$query = "INSERT INTO casos(nivel1, proxima, peso) VALUES('$nivel1', '$proximo', 1)";				
 	}
 	
 	$conexao->executa($query);
@@ -35,7 +34,7 @@ else if($tipo == 2){
 		$query = "UPDATE casos SET peso = peso+2 WHERE id=$id";
 	}
 	else{
-		$query = "INSERT INTO casos(nivel2, nivel1, proxima, peso) VALUES('$nivel2', '$nivel1', '$proximo', 2)";		$xml= simplexml_load_file("xml_casos.xml");		$caso = $xml->addChild("caso");		$caso->addChild("nivel1",$nivel1);		$caso->addChild("nivel2",$nivel2);		$caso->addChild("peso",2);		$caso->addChild("proxima",$proximo);		file_put_contents("xml_casos.xml",$xml->asXML());
+		$query = "INSERT INTO casos(nivel2, nivel1, proxima, peso) VALUES('$nivel2', '$nivel1', '$proximo', 2)";		
 	}
 	
 	$conexao->executa($query);
@@ -55,7 +54,7 @@ else if($tipo == 3){
 		$query = "UPDATE casos SET peso = peso+3 WHERE id=$id";
 	}
 	else{
-		$query = "INSERT INTO casos(nivel3, nivel2, nivel1, proxima, peso) VALUES('$nivel3', '$nivel2', '$nivel1', '$proximo', 3)";				$xml= simplexml_load_file("xml_casos.xml");		$caso = $xml->addChild("caso");		$caso->addChild("nivel1",$nivel1);		$caso->addChild("nivel2",$nivel2);		$caso->addChild("nivel3",$nivel3);		$caso->addChild("peso",3);		$caso->addChild("proxima",$proximo);		file_put_contents("xml_casos.xml",$xml->asXML());
+		$query = "INSERT INTO casos(nivel3, nivel2, nivel1, proxima, peso) VALUES('$nivel3', '$nivel2', '$nivel1', '$proximo', 3)";				
 	}
 	
 	$conexao->executa($query);
--- login.php
+++ login.php
@@ -5,6 +5,21 @@ require_once("MySQL.class.php");
 if(isset($_POST['botao'])){
 	
 	$conexao = new MySQL();
+	$select = "SELECT id FROM casos";
+	$exec = $conexao->consulta($select);
+	if(count($exec) == 0){
+		$xml= simplexml_load_file("ajax/xml_casos.xml");
+		
+		foreach($xml as $caso){
+			$nivel1 = (string)$caso->nivel1;
+			$nivel2 = (string)$caso->nivel2;
+			$nivel3 = (string)$caso->nivel3;
+			$prox 	= (string)$caso->proxima;
+			$peso	= (string)$caso->peso;
+			$insert = "INSERT INTO casos(nivel3,nivel2,nivel1,proxima,peso) VALUES('".$nivel3."','".$nivel2."','".$nivel1."','".$prox."','".$peso."')";
+			$xml_inserted_cases = $conexao->executa($insert);
+		}
+	}
 	$query = "SELECT id FROM usuarios WHERE login='".$_POST['login']."' AND senha='".md5($_POST['senha'])."'";
 	
 	$resultado = $conexao->consulta($query);
